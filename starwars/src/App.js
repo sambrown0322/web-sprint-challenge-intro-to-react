bb57c4a33855e46eda8b102b75cb8078adde2814
David Brown <davidbrown@Davids-iMac.local>
2020-07-17 11:30:23 -0600

    mvp plus animations

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Character from "./components/Character";
+import styled from "styled-components";
+
+const StyleH1 = styled.h1`
+  font-size: 2.5rem;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characterInfo, setCharacterInfo] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then((res) => {
+        setCharacterInfo(res.data.results);
+        // console.log(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <StyleH1 className="Header">Characters</StyleH1>
+      <Character characterInfo={characterInfo} />
     </div>
   );
-}
+};
 
 export default App;
